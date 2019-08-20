d62ae04cab8fb21fc1ec1174d9aa3d8ef005e9b8
mememeil <kodhikodhi@gmail.com>
2019-08-21 01:08:47 +0900

    編集ボタンを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,16 @@ class UsersController < ApplicationController
     user.destroy
     redirect_to "/"
   end
+
+  def edit
+    @user = User.find(params["id"])
+  end
+
+  def update
+    user = User.find(params["id"])
+    user.name = params["user"]["name"]
+    user.age = params["user"]["age"]
+    user.save
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "users#index"
   post "/create", to: "users#create"
   delete "/user/:id", to: "users#destroy"
+  get "/user/:id/edit", to: "users#edit"
+  patch "/user/:id", to: "users#update"
 end
